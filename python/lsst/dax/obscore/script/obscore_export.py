325885c7d58da2cf18bb78998e2f84be77818e0a
Andy Salnikov <salnikov@slac.stanford.edu>
2022-04-21 22:14:52 -0700

    WIP: Start implementing parquet exporter

---
+++ python/lsst/dax/obscore/script/obscore_export.py
@@ -0,0 +1,48 @@
+# This file is part of dax_obscore.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (http://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+__all__ = ["obscore_export"]
+
+import yaml
+
+from lsst.daf.butler import Butler
+
+from .. import ExporterConfig, ObscoreExporter
+
+
+def obscore_export(repo: str, destination: str, config: str) -> None:
+    """Export Butler datasets as ObsCore Data Model in parquet format.
+
+    Parameters
+    ----------
+    repo : `str`
+        URI to the butler repository.
+    destination : `str`
+        Location of the output file.
+    config : `str`
+        Location of the configuration file.
+    """
+    butler = Butler(repo)
+    with open(config) as stream:
+        config_data = yaml.safe_load(stream)
+    cfg = ExporterConfig.parse_obj(config_data)
+    exporter = ObscoreExporter(butler, cfg)
+    exporter.to_parquet(destination)
