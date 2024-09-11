dafd68a1d36581a8445cec17c677512e485bf905
Tim Jenness <tjenness@lsst.org>
2024-09-11 14:43:34 -0700

    Do not allow all butler collections to be searched

--- python/lsst/dax/obscore/obscore_exporter.py
+++ python/lsst/dax/obscore/obscore_exporter.py
@@ -477,7 +477,7 @@ class ObscoreExporter:
 
         collections: Any = self.config.collections
         if not collections:
-            collections = "*"
+            raise ValueError("No collections specified. Querying all collections is not allowed.")
 
         # Have to use non-public Registry interface.
         registry = self.butler._registry  # type: ignore
