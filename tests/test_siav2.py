dfbeb70da544e62e70f98c0cb11b26c2b4125895
Tim Jenness <tjenness@lsst.org>
2024-09-30 15:05:13 -0700

    Explicitly copy the config for testing before setting batch size

--- tests/test_siav2.py
+++ tests/test_siav2.py
@@ -172,7 +172,7 @@ class SIAv2TestCase(unittest.TestCase, DaxObsCoreTestMixin):
 
     def test_query(self):
         """Test that an SIAv2 query completes."""
-        config = self.config
+        config = self.config.copy()
         config.batch_size = 3
         for kwargs, expected in (
             ({"instrument": "HSC"}, 68),
